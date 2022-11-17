dcd02b6c27b0d75b0b6eb5a4cbf9530968cf1a4f
unknown <pranavterkar@gmail.com>
2022-11-17 16:18:56 +0530

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar';
+import Tital from './Tital';
+const Header = () => {
+  return (
+    <div>
+    <Tital/>
+    <Navbar/>
+
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tital = () => {
+  return (
+    <div>
+        <h1>AlgoHelp</h1>
+    </div>
+  )
+}
+
+export default Tital;
